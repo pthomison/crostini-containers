55fe1a4a318a396ce4b116e5c5e42485b40f7a0d
Patrick Thomison <p.thomison@gmail.com>
2020-08-14 11:02:06 -0700

    Update debian-init.sh

--- debian-init.sh
+++ debian-init.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
+# getting latest
 apt-get update && apt-get upgrade -y
 
+# docker
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+ 
+ add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+apt-get install -y nmap
