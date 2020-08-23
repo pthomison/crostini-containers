08f653b33b803cb4104b8782852741d8cbbeff5d
Patrick Thomison <p.thomison@gmail.com>
2020-08-23 07:30:36 -0700

    Update cut-debian-container.sh

--- cut-debian-container.sh
+++ cut-debian-container.sh
@@ -6,8 +6,4 @@ USERNAME=pthomison
 
 lxc delete -f $VM_NAME
 
-lxc launch images:debian/bullseye $VM_NAME
-
-lxc exec penguin -- bash -c "apt-get update && apt-get upgrade -y && apt-get install curl -y"
-
-lxc exec penguin -- bash -c 'curl https://raw.githubusercontent.com/pthomison/test-provisioning/master/debian-init.sh | bash'
+lxc launch images:debian/bullseye $VM_NAME < /dev/null
