5f8c8783b4424cc4ab3a75591753a339adc22f25
Patrick Thomison <p.thomison@gmail.com>
2020-08-23 07:26:24 -0700

    Update debian-init.sh

--- debian-init.sh
+++ debian-init.sh
@@ -11,7 +11,7 @@ apt-get install -y \
     gnupg-agent \
     software-properties-common
     
- curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
  
  add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
