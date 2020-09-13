a8ecd3c8aee0463973259407f783b92843c0e8b7
Patrick Thomison <p.thomison@gmail.com>
2020-09-13 10:20:12 -0700

    Update fedora-32.sh

--- fedora-32.sh
+++ fedora-32.sh
@@ -84,27 +84,25 @@ ln -s /mnt/chromeos/MyFiles/Configs/ssh/config /home/pthomison/.ssh/config
 ln -s /mnt/chromeos/MyFiles/Configs/aws/config /home/pthomison/.aws/config
 ln -s /mnt/chromeos/MyFiles/Configs/aws/credentials /home/pthomison/.aws/credentials
 
-sudo su - $USERNAME
+# sudo su - $USERNAME
 
-whoami
+# whoami
 
-journalctl -e --no-pager
+# journalctl -e --no-pager
 
-systemctl --failed --no-pager --all
+# systemctl --failed --no-pager --all
 
-systemctl --user enable --now sommelier@0 
-systemctl --user enable --now sommelier-x@0
-systemctl --user enable --now sommelier@1
-systemctl --user enable --now sommelier-x@1
-systemctl --user enable --now cros-garcon
+su $USERNAME -c "systemctl --user enable --now sommelier@0"
+su $USERNAME -c "systemctl --user enable --now sommelier-x@0"
+su $USERNAME -c "systemctl --user enable --now sommelier@1"
+su $USERNAME -c "systemctl --user enable --now sommelier-x@1"
+su $USERNAME -c "systemctl --user enable --now cros-garcon"
 
 #sommelier-x@0 sommelier@1 sommelier-x@1 cros-garcon cros-pulse-config
 # systemctl --user enable --now cros-pulse-config
 
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sudo su -
+su $USERNAME -c 'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'
 
 chsh $USERNAME -s $(which zsh)
 
