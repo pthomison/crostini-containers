f4ac85f3f7661d04aeafb44bd70ddcfdb4e396ec
Patrick Thomison <p.thomison@gmail.com>
2020-09-18 13:21:53 -0700

    Create fedora-kubernetes.sh

---
+++ fedora-kubernetes.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -xe
+VM_NAME=penguin
+
+cat << 'EOD' > /tmp/kubernetes.sh
+#!/usr/bin/env bash
+set -xe
+USERNAME="pthomison"
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl --system
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
+
+
+EOD
+
+sleep 2
+
+lxc file push /tmp/k8s.sh penguin/tmp/k8s.sh
+
+sleep 2
+
+lxc exec penguin -- bash -c "bash /tmp/k8s.sh"
